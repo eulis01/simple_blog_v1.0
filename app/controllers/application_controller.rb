24ee848728aefba80dc53fdf9cd0a58ccfd4f2d0
eulis01 <eulis01@gmail.com>
2020-10-20 22:54:47 -0400

    added helpers to application class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,27 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secrect, ENV['SESSION_SECRECT']
   end
 
   get "/" do
     erb :welcome
   end
 
+  helpers do
+    def current_user 
+      @current_user ||= User.find_by_id(session[:user_id])
+    end
+
+    def is_authorized?(record, path)
+      unless record.user == current_user
+      redirect path
+      end
+    end
+
+    def logged_in?
+    !!current_user
+    end
+  end
 end
