0bf8980bf8efe5cde792f983fe714dd2e6b22990
eulis01 <eulis01@gmail.com>
2020-10-24 19:04:25 -0400

    user has_many posts associated with posts model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
+# we inherited from ActiveRecord does most of the initializations work for us.
 class User < ActiveRecord::Base
+  has_many :posts # associations to posts established.
+  # using bcrypt macro to hash and secure the password
+  has_secure_password
 end
