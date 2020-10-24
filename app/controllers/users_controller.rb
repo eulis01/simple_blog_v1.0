016092d04127d205a7d0446c482d8fbbb5f58da7
eulis01 <eulis01@gmail.com>
2020-10-24 18:52:49 -0400

    generated user model with views and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,66 +1,22 @@
 class UsersController < ApplicationController
-  # Creating a new User 
-  get "/signup" do
-    erb :"/users/new.html"
-  end
-
-  # POST: /signup
-  post "/signup" do
-    # Checking if there are any blank fields
-    if params[:user].values.any? { |v|v.blank?}
-      redirect "/signup"
-      # Checking that the Username is Available
-    elsif User.find_by(username: params[:user][:username])
-      redirect "/login"
-    else
-      # Create a New User
-      @user = User.create(params[:user])
-      # Log User in,  and verified the session hash mached the user_id
-      session[:user_id] = @user.id
-      redirect "/users"
-    end
-  end
 
   # GET: /users
   get "/users" do
-    @user = current_user
-    erb :"/users/show.html"
-  end
-  
-  get "/logout" do
-    session.clear
-    redirect "/"
+    erb :"/users/index.html"
   end
 
-  # GET: /users/login
-  get "/users/login" do
-    erb :"/users/login.html"
+  # GET: /users/new
+  get "/users/new" do
+    erb :"/users/new.html"
   end
 
-  # POST: /login
-  post "/login" do
-    #  Does the User has a Valid username?
-    user = User.find_by(username: params[:user][:username])
-
-    if user && user.authenticate(params[:user][:password])
-      # User succesifully Log in!!.
-      session[:user_id] = user.id
-
+  # POST: /users
+  post "/users" do
     redirect "/users"
-    else
-      redirect 
-      "/login"
-    end
-  end
-
-  get "/logout" do
-    session.destroy
-    redirect "/"
   end
 
   # GET: /users/5
   get "/users/:id" do
-    @user = User.find_by(id: params[:id])
     erb :"/users/show.html"
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,2 @@
 class User < ActiveRecord::Base
-  has_many :posts
-  # => using ActiveRecord macro to do validations.
-  has_secure_password 
 end
