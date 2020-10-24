ea29d3fc3fe4bd7d5e31fca3ae00b39a55671034
eulis01 <eulis01@gmail.com>
2020-10-24 19:16:44 -0400

    added atributes for creating users

--- db/migrate/20201024225022_create_users.rb
+++ db/migrate/20201024225022_create_users.rb
@@ -1,7 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string :name
+      t.text :bio
+      t.string :image_url
+      t.string :email
+      t.string :password_digest
       t.timestamps null: false
     end
   end
