ab551cc4fdb643fd5e618b81920be8f5e394c8f3
eulis01 <eulis01@gmail.com>
2020-11-05 19:08:35 -0500

    added user login account logic to routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,8 @@
 class UsersController < ApplicationController
 
-
   get "/login" do
     erb :"/users/login"
   end
-
 
   post "/login" do
     @user = User.find_by(email: params[:email])
@@ -21,26 +19,37 @@ class UsersController < ApplicationController
 
 
   get "/users/:id" do
-    @user = User.find_by(id: params[:id])
-    erb :"/users/show"
+    if logged_in? && User.find_by_id(params[:id])
+      @user = User.find_by_id(params[:id])
+      erb :"/users/show"
+    else
+      redirect_if_not_logged_in
+    end
   end
-
 
   get "/signup" do
     erb :"/users/signup"
   end
 
-
   post "/users" do
     @user = User.create(params)
+    if @user.save
+      flash[:message] = "Welcome To SimpleBlog 😃"
     session[:user_id] = @user.id
-
-    redirect "/users/#{@user.id}"
+    redirect to "/users/#{@user.id}"
+    else
+      flash[:error] = "[#{@user.errors.full_messages.to_sentence}]!!"
+      redirect to "/"
+    end
   end
 
 
   get "/logout" do
-    session.clear
-    redirect "/"
+    if logged_in?
+      session.clear
+      redirect to "/login"
+    else
+      redirect to "/"
+    end
   end
 end
