2014b997d51033fb91da63c9b462abc51f4663b0
eulis01 <eulis01@gmail.com>
2020-11-06 11:00:50 -0500

    add comment explaining Validations of users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
-# we inherited from ActiveRecord does most of the initializations work for us.
+# We inherited from ActiveRecord does most of the initializations work for us.
 class User < ActiveRecord::Base
+  # Validating the user :name, :email and the presence of the input.
   validates :name, :email, presence: true
   validates_uniqueness_of :name, :email, case_sensitive: false
-  has_many :posts # associations to posts established.
-
+  # associations to posts established.
+  has_many :posts 
+  # Bcrypt gem used to Hash and Salt the Password.
   has_secure_password
 end
