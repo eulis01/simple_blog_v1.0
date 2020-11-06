55506e1425b4ba095a9b9f2a7b69296415d1aa5e
eulis01 <eulis01@gmail.com>
2020-11-05 19:07:13 -0500

    added validation for user name and email  model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 # we inherited from ActiveRecord does most of the initializations work for us.
 class User < ActiveRecord::Base
+  validates :name, :email, presence: true
+  validates_uniqueness_of :name, :email, case_sensitive: false
   has_many :posts # associations to posts established.
 
   has_secure_password
-  validates :email, uniqueness: true
 end
